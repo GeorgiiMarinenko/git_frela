84c9d7357bdfb9d8e58688a5f18b3a4010b6fda5
Георгий Мариненко <gmarinenko@mail.ru>
2022-04-21 14:58:18 +0300

    исправление конфликтов в main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 
 int main() {
-<<<<<<< HEAD
-    std::cout << "Hello all" << std::endl;
-=======
     int a, b;
     a = 5;
     b = 5;
     std::cout << "res = " << a + b << std::endl;
->>>>>>> e8851b9c0edd772d5eaae9969247256cc7e2c65d
 
     return 0;
 }
