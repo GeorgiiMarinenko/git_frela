1d220d0c369efc1e0e57b2a6e0fefd34cd79a9b8
denis <mail@mail.ru>
2022-04-28 17:03:50 +0300

    Доделанные тесты

--- testCatch/testEng.cpp
+++ testCatch/testEng.cpp
@@ -1,14 +1,14 @@
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_all.hpp>
 #include "TEngCalc.h"
 
 TEST_CASE ("SumEngTest are computed", "[sumTest]") {
     TEngCalc exmpl;
-    REQUIRE(exmpl.sum(5, 4) == 9); 
+    REQUIRE(exmpl.sum(5.5, 4.6) == Catch::Approx(10.1)); 
 }
 
 TEST_CASE ("SubEngTest are computed", "[subTest]") {
     TEngCalc exmpl;
-    REQUIRE(exmpl.sub(5, 4) == 1); 
+    REQUIRE(exmpl.sub(5.5, 4.1) ==  Catch::Approx(1.4)); 
 }
 
 TEST_CASE ("SubEngTest are computed", "[squareRTest]") {
