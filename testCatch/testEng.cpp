55103207c017c60e8757e325ccb2b4a7ce21b94c
denis <mail@mail.ru>
2022-04-28 17:08:00 +0300

    Удаление мусора

--- testCatch/testEng.cpp
+++ testCatch/testEng.cpp
@@ -1,17 +1,18 @@
 #include <catch2/catch_all.hpp>
+
 #include "TEngCalc.h"
 
-TEST_CASE ("SumEngTest are computed", "[sumTest]") {
+TEST_CASE("SumEngTest are computed", "[sumTest]") {
     TEngCalc exmpl;
-    REQUIRE(exmpl.sum(5.5, 4.6) == Catch::Approx(10.1)); 
+    REQUIRE(exmpl.sum(5.5, 4.6) == Catch::Approx(10.1));
 }
 
-TEST_CASE ("SubEngTest are computed", "[subTest]") {
+TEST_CASE("SubEngTest are computed", "[subTest]") {
     TEngCalc exmpl;
-    REQUIRE(exmpl.sub(5.5, 4.1) ==  Catch::Approx(1.4)); 
+    REQUIRE(exmpl.sub(5.5, 4.1) == Catch::Approx(1.4));
 }
 
-TEST_CASE ("SubEngTest are computed", "[squareRTest]") {
+TEST_CASE("SubEngTest are computed", "[squareRTest]") {
     TEngCalc exmpl;
-    REQUIRE(exmpl.squareR(4) == 2); 
+    REQUIRE(exmpl.squareR(4) == 2);
 }
